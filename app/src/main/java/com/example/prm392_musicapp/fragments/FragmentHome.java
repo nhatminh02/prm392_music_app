68696f6315a95b07d9107965b97920d2ec3810b2
Phạm Nhật Minh <113897170+nhatminh02@users.noreply.github.com>
2023-06-16 16:49:54 +0700

    Add transactionFragment function
    Add transactionFragment function

--- app/src/main/java/com/example/prm392_musicapp/fragments/FragmentHome.java
+++ app/src/main/java/com/example/prm392_musicapp/fragments/FragmentHome.java
@@ -58,14 +58,10 @@ public class FragmentHome extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
 
     @Override
