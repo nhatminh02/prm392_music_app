4c21e3aa7b8ea5536ee36259dab023a78d4db60f
Tskiser <113109529+Tskiser@users.noreply.github.com>
2023-06-22 21:42:55 +0700

    gay setting page
    gay

---
+++ app/src/main/java/com/example/prm392_musicapp/Settnoti.java
@@ -0,0 +1,14 @@
+package com.example.prm392_musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settnoti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settnoti);
+    }
+}
