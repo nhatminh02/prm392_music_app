75f3dced5a01f19f94bd7088563684e32ee7dad6
Phạm Nhật Minh <113897170+nhatminh02@users.noreply.github.com>
2023-07-02 09:40:43 +0700

    Add change theme function
    Add change theme function

---
+++ app/src/main/java/com/example/prm392_musicapp/activities/SettingThemeActivity.java
@@ -0,0 +1,49 @@
+package com.example.prm392_musicapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.example.prm392_musicapp.R;
+
+public class SettingThemeActivity extends AppCompatActivity {
+    Switch switchThemeBtn;
+    boolean darkMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme_setting);
+//        getSupportActionBar().hide();
+        switchThemeBtn = findViewById(R.id.switch_theme);
+
+        sharedPreferences = getSharedPreferences("mode", Context.MODE_PRIVATE);
+        darkMode = sharedPreferences.getBoolean("dark", false);
+
+        if(darkMode){
+            switchThemeBtn.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+    }
+
+    public void handleSwitch(View v){
+        if(darkMode){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            editor = sharedPreferences.edit();
+            editor.putBoolean("dark", false);
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            editor = sharedPreferences.edit();
+            editor.putBoolean("dark", true);
+        }
+        editor.apply();
+    }
+}
