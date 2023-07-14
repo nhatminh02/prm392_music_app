d9d85ed7126c513d269f40edd381dc58a139764d
Kaizoku0u <shikamaru.nara2k2@gmail.com>
2023-07-14 23:40:30 +0700

    Update FragmentPlaylist.java

--- app/src/main/java/com/example/prm392_musicapp/fragments/FragmentPlaylist.java
+++ app/src/main/java/com/example/prm392_musicapp/fragments/FragmentPlaylist.java
@@ -93,7 +93,7 @@ public class FragmentPlaylist extends Fragment {
         ((Button) view.findViewById(R.id.btn_ctrPlaylist)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                    
             }
         });
 
