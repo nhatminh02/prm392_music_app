4f426894c8a036bd60a22694042a8a18e3a52f42
ZellyFish18 <110874893+ZellyFish18@users.noreply.github.com>
2023-07-16 11:28:33 +0700

    Add on click video play in Liked Tracks

--- app/src/main/java/com/example/prm392_musicapp/adapter/LikedMusicAdapter.java
+++ app/src/main/java/com/example/prm392_musicapp/adapter/LikedMusicAdapter.java
@@ -1,6 +1,7 @@
 package com.example.prm392_musicapp.adapter;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,18 +23,19 @@ import java.util.List;
 public class LikedMusicAdapter extends RecyclerView.Adapter<LikedMusicAdapter.MusicHolder> {
     private final List<Video> dataList;
     String videoId;
-    private View.OnClickListener onClickListener;
+    private LikedMusicAdapter.OnItemClickListener listener;
     MySQLiteOpenHelper mySQLiteOpenHelper;
+
+    public interface OnItemClickListener {
+        void onItemClick(String itemId);
+    }
+
+    public void setOnItemClickListener(LikedMusicAdapter.OnItemClickListener listener) {
+        this.listener = listener;
+    }
 
     public LikedMusicAdapter(List<Video> dataList) {
         this.dataList = dataList;
-    }
-    public void setOnClickListener(View.OnClickListener onClickListener) {
-        this.onClickListener = onClickListener;
-    }
-
-    public interface OnClickListener {
-        void onClick(int position, Video video);
     }
 
     @NonNull
@@ -60,7 +62,7 @@ public class LikedMusicAdapter extends RecyclerView.Adapter<LikedMusicAdapter.Mu
         return dataList.size();
     }
 
-    public class MusicHolder extends RecyclerView.ViewHolder {
+    public class MusicHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView imv_thumb;
         TextView tv_musname;
         TextView tv_singer;
@@ -68,6 +70,7 @@ public class LikedMusicAdapter extends RecyclerView.Adapter<LikedMusicAdapter.Mu
 
         public MusicHolder(@NonNull View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
             imv_thumb = itemView.findViewById(R.id.imv_thumb);
             tv_musname = itemView.findViewById(R.id.tv_musname);
             tv_singer = itemView.findViewById(R.id.tv_singer);
@@ -85,6 +88,16 @@ public class LikedMusicAdapter extends RecyclerView.Adapter<LikedMusicAdapter.Mu
                     notifyItemRemoved(position);
                 }
             });
+        }
+
+        public void onClick(View v) {
+            int position = getBindingAdapterPosition();
+            Log.i("LikedMusicAdapter", "Item clicked");
+            if (position != RecyclerView.NO_POSITION && listener != null) {
+                Video video = dataList.get(position);
+                String videoId = video.getVideoId(); // Retrieve the ID of the clicked item
+                listener.onItemClick(videoId); // Callback the listener with the item ID
+            }
         }
     }
 }
